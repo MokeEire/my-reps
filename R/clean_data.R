ce7714ba2b0e93c75bfd72d6fec9dcebd516050f
MokeEire <markkbarrett@gmail.com>
2022-06-27 23:53:55 -0700

    Remove functionalized actions

--- R/clean_data.R
+++ R/clean_data.R
@@ -35,40 +35,6 @@ toc()
 
 saveRDS(all_bills, here("data", "cleaned", "BILLSTATUS_117_House.Rds"))
 
-actions_unnested = select(all_bills, where(negate(is_list)), starts_with("action")) %>% 
-  # Unnest actions
-  unnest(actions) %>% 
-  # Create action timestamp
-  mutate(action_ts = make_datetime(year = year(actionDate), 
-                                   month = month(actionDate), day = day(actionDate), 
-                                   hour = coalesce(hour(actionTime), 0), 
-                                   min = coalesce(minute(actionTime), 0), 
-                                   sec = coalesce(second(actionTime), 0), 
-                                   tz = "US/Eastern"),
-         action_type = fct_explicit_na(
-           factor(action_type, 
-                  levels = c("IntroReferral", "Committee", "Floor", 
-                             "Discharge", "President", "BecameLaw"), 
-                  ordered = T),
-           na_level = "(Missing Action Type)"
-         ),
-         action_source_name = factor(action_source_name,
-                                     levels = c("Library of Congress", "House floor actions", 
-                                                "House committee actions", "Senate"),
-                                     ordered = T)) %>% 
-  
-  # Join in action codes
-  left_join(action_codes, by = c("actionCode" = "Code")) %>% 
-  # Order bills and actions
-  arrange(bill_id, action_ts, action_type, action_source_name) %>% 
-  # Number actions
-  group_by(billType, billNumber) %>% 
-  # Number actions
-  mutate(action_number = row_number(),
-         became_law = ("BecameLaw" %in% action_type), .after = actionTime) %>% 
-  ungroup()
+actions_unnested = unnest(all_bills_coded, actions)
 
-actions_unnested_clean = actions_unnested %>% 
-  # Remove Intro-H action codes (keep NAs)
-  filter(replace_na(actionCode != "Intro-H", T))
 
