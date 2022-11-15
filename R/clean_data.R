e971adf665d0535f49e6ff06004297d026dc0306
MokeEire <markkbarrett@gmail.com>
2022-11-15 10:13:27 -0800

    rename clean_data -> collect_data and add new clean_data file

--- R/clean_data.R
+++ R/clean_data.R
@@ -3,46 +3,62 @@ library(tictoc)
 library(furrr)
 library(here)
 library(reactable)
+library(reactablefmtr)
 plan(multisession)
 source("R/parsing_functions.R")
 
-
-
 action_codes = read_csv(here("data", "action_codes.csv"), col_types = "cc")
 
-# List files ----
-
-bill_types = list.files(here("data", "BILLSTATUS","117"))
-
-bill_folders = here("data", "BILLSTATUS", "117", bill_types) %>% 
-    str_remove_all("\\/OneDrive")
-
-bill_files = map(bill_folders, list.files, full.names = T) %>% 
-  set_names(bill_types)
-
-all_files = flatten_chr(bill_files)
-
-
-# Extract bills from files ------------------------------------------------
-
-tic(str_c("Extract ", length(all_files), " bills"))
-all_bills = future_map_dfr(all_files, extract_bill_status, log_types = "console")
-toc()
-
+# Load cleaned R objects
+all_bills = readRDS(here("data", "cleaned", "BILLSTATUS_117_2022-11-10.Rds"))
 
 # Clean bills -------------------------------------------------------------
 
+actions_unnested = select(all_bills, 
+       bill_id, title, bill_type, actions) %>% 
+  # Unnest actions
+  unnest(actions)
+
+actions_joined = actions_unnested %>% 
+  # Join actions to the list of action codes
+  left_join(action_codes, by = "action_code")
+
+intro_actions = actions_joined %>% 
+  filter(action_type == "IntroReferral")
+
+intro_action_codes = tribble(
+  ~action_code, ~action_code_desc,
+  "1000", "Introduced in House",
+  "Intro-H", "Introduced in House",
+  "10000", "Introduced in Senate",
+  "B00100", "Sponsor introductory remarks on measure",
+  "H11100", "Referred to Committee",
+  "H11210", "Committee granted an extension for further consideration"
+)
+
+intro_actions_coded = intro_actions %>%
+  # Join action codes
+  left_join(intro_action_codes, by = "action_code") %>% 
+  # Assign actions to category based on their text when missing action code
+  mutate(action_code_desc = case_when(
+    !is.na(action_code_desc) ~ action_code_desc,
+    str_detect(action_text, "[Rr]eferred to the (Select\\s)?Committee") ~ "Referred to Committee",
+    str_detect(action_text, "Received in the Senate") ~ "Received in the Senate"
+  ))
+
+intro_actions_dedupe = intro_actions_coded %>% 
+  # Remove Intro-H action codes (keep NAs)
+  filter(replace_na(action_code != "Intro-H", T)
+
 tic("Code all bills")
 all_bills_coded = mutate(all_bills,
+                         bill_type_cat = case_when(bill_type %in% c("HR", "S") ~ "Bill",
+                                                   bill_type %in% c("HJRES", "SJRES") ~ "Joint Resolution",
+                                                   bill_type %in% c("HRES", "SRES") ~ "Simple Resolution",
+                                                   bill_type %in% c("HCONRES", "SCONRES") ~ "Concurrent Resolution"),
                          actions = future_map(actions, remove_duplicate_actions),
                          actions = future_map(actions, code_actions, action_codes = action_codes))
 toc()
 
 
 actions_unnested = unnest(all_bills_coded, actions)
-
-
-# Save objects ------------------------------------------------------------
-
-saveRDS(all_bills, here("data", "cleaned", "BILLSTATUS_117.Rds"))
-saveRDS(actions_unnested, here("data", "cleaned", "BILLSTATUS_117_Actions.Rds"))
---
+++ R/collect_data.R
@@ -0,0 +1,63 @@
+library(extrafont)
+library(tictoc)
+library(furrr)
+library(here)
+library(reactable)
+plan(multisession)
+source("R/parsing_functions.R")
+
+
+
+action_codes = read_csv(here("data", "action_codes.csv"), col_types = "cc")
+
+# List files ----
+
+bill_types = list.files(here("data", "BILLSTATUS","117"))
+
+bill_folders = here("data", "BILLSTATUS", "117", bill_types) %>% 
+    str_remove_all("\\/OneDrive")
+
+bill_files = map(bill_folders, list.files, full.names = T) %>% 
+  set_names(bill_types)
+
+all_files = flatten_chr(bill_files)
+
+
+# Get files from sitemap --------------------------------------------------
+# bill_status_sitemaps = read_xml("https://www.govinfo.gov/sitemap/bulkdata/BILLSTATUS/sitemapindex.xml") %>% 
+#   # Strip namespace for coherent xpaths
+#   xml_ns_strip() %>% 
+#   # Find all loc elements which contain urls
+#   xml_find_all("/sitemapindex/sitemap/child::loc") %>% 
+#   # Convert them to text
+#   map_chr(xml_text)
+
+# Select sitemaps for given congress
+# sitemaps_117 = str_subset(bill_status_sitemaps, "117")
+# 
+# (xml_nodes_117 = sitemaps_117 %>% 
+#     map(read_xml) %>% 
+#     map(xml_ns_strip) %>% 
+#     map(xml_find_all, "/urlset/url/child::loc"))
+# 
+# xml_files_117 = xml_nodes_117 %>% 
+#   map(xml_text) %>% 
+#   set_names(str_extract(sitemaps_117, "(?<=BILLSTATUS\\/117)[a-z]{1,7}"))
+# 
+# xml_files_117_sample = map(xml_files_117, sample, size = 10)
+
+
+# Extract bills from files ------------------------------------------------
+
+# Testing
+# map_dfr(bill_files$hres, extract_bill_status, log_types = "console")
+tic(str_c("Extract ", length(all_files), " bills"))
+all_bills = future_map_dfr(all_files, extract_bill_status, log_types = NULL)
+toc()
+
+
+
+# Save objects ------------------------------------------------------------
+
+saveRDS(all_bills, here("data", "cleaned", paste0("BILLSTATUS_117_", lubridate::today(), ".Rds")))
+# saveRDS(actions_unnested, here("data", "cleaned", "BILLSTATUS_117_Actions.Rds"))
